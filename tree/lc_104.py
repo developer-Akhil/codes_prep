a34ba0878b49a1f7353004348786ed1d8ce59368
Akhilesh Chand <64408106+developer-Akhil@users.noreply.github.com>
2024-10-15 23:03:45 +0530

    Update lc_104.py

--- tree/lc_104.py
+++ tree/lc_104.py
@@ -3,7 +3,6 @@
 # Vedios : https://www.youtube.com/watch?v=hTM3phVI6YQ
 
 # Given the root of a binary tree, return its maximum depth.
-#
 # A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
 
